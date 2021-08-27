1f8f90da4fab5a2e7730591cd9407163b547db23
teddy1565 <teddy1565@github.com>
2021-08-27 17:23:34 +0800

    server.js

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+const {PORT=3000}=process.env;
+//const IS_TEST = !!module.parent;
+
+app.get("/",(req,res)=>{
+	res.status(200).send("Hello CI/CD!");
+});
+
+//if(!IS_TEST){
+	app.listen(PORT,()=>{
+		console.log("Server is running on PORT:",PORT);
+	});
+//}
+export default app;
+
